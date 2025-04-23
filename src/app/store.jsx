8e78f1c9424cd0a41c378149bbdc9ce720b58506
himanshutamta <himanshutamta43w@gmail.com>
2025-04-24 00:54:31 +0530

    after resolve error

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -6,6 +6,7 @@ import storage from "redux-persist/lib/storage"
 const persistConfig = {
     key: "root",
     storage,
+    blacklist : []
 };
 
 const persistedReducer = persistReducer(persistConfig, productSlice.reducer);
@@ -13,8 +14,15 @@ const persistedReducer = persistReducer(persistConfig, productSlice.reducer);
   
 export const store = configureStore({
     reducer: {
-        product : persistedReducer
-    }
+        product: persistedReducer,   
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+            ignoredPaths: ["register"], // Ignore paths containing non-serializable values
+          },
+        }),
 })
 export const persistor = persistStore(store);
 
